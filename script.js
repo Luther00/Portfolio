fe566ec1d84a901bc794aa06e8586ccd6c0d60f3
Luther00 <77666004+Luther00@users.noreply.github.com>
2021-07-07 13:21:39 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -35,18 +35,19 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Developer","Designer", "Freelancer"],
+        strings: ["Website Developer","Graphic Designer"],
         typeSpeed: 100,
-        backSpeed: 60,
+        backSpeed: 100,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Developer","Designer", "Freelancer"],
+        strings: ["Website Developer","Graphic Designer"],
         typeSpeed: 100,
-        backSpeed: 60,
+        backSpeed: 100,
         loop: true
     });
+
 
     // owl carousel script
     $('.carousel').owlCarousel({
